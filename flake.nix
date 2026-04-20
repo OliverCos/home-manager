820eb11760f75b7241f479574a03605b788d4d6b
OliverCos <oliver.cosgrove@oriolenetworks.com>
2026-04-20 12:16:23 +0100

    Remove GUI elements

--- flake.nix
+++ flake.nix
@@ -9,15 +9,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    zen-browser = {
-      url = "github:youwen5/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
   };
 
-  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
-    homeConfigurations."oliver" = home-manager.lib.homeManagerConfiguration {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    homeConfigurations."oliverc" = home-manager.lib.homeManagerConfiguration {
       pkgs = import nixpkgs {
         system = "x86_64-linux";
         config.allowUnfree = true;
--- home.nix
+++ home.nix
@@ -3,25 +3,24 @@
 
 {
   imports = [
-    ./configs/i3.nix
-    ./configs/i3status-rust.nix
-    ./configs/rofi.nix
-    ./configs/dunst.nix
-    ./configs/kitty.nix
     ./configs/zsh.nix
     ./configs/btop.nix
     ./configs/fastfetch.nix
-    ./configs/gtk.nix
-    ./configs/gnome-terminal.nix
-    ./configs/xterm.nix
     ./configs/neovim.nix
   ];
 
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
+
   home = {
-    username = "oliver";
-    homeDirectory = "/home/oliver";
+    username = "oliverc";
+    homeDirectory = "/home/oliverc";
     stateVersion = "25.11";
-    sessionPath = [ 
+    sessionPath = [
       "/opt/2025.1/Vivado/bin"
       "/opt/questasim/bin"
       "/opt/2025.1/Vitis/bin"
@@ -32,7 +31,7 @@
       LANG = "en_US.UTF-8";
     };
   };
-  xdg.cacheHome = "/scratch/oliver/.cache";
+  xdg.cacheHome = "/scratch/oliverc/.cache";
 
   home.file.".bashrc".text = ''
     # If running interactively, switch to zsh
@@ -42,51 +41,10 @@
     fi
   '';
 
-  home.file.".xprofile".text = ''
-    [ -f ~/.Xresources ] && xrdb -merge ~/.Xresources
-  '';
-
-  home.file."startwm.sh" = {
-    executable = true;
-    text = ''
-      #!/bin/sh
-      [ -f ~/.xprofile ] && . ~/.xprofile
-      export XDG_DATA_DIRS="$HOME/.nix-profile/share:$HOME/.local/share:/usr/share:/usr/local/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
-      export PATH="$HOME/.nix-profile/bin:$PATH"
-      exec $HOME/.nix-profile/bin/i3
-    '';
-  };
-
-  fonts.fontconfig.enable = true;
-
   home.packages = with pkgs; [
-    # Terminal & tools
     tmux
     fzf
     fastfetch
-
-    # i3 ecosystem
-    i3
-    i3status-rust
-    rofi
-    dunst
-    feh
-    libnotify
-
-    # Theming
-    glib
-    (graphite-gtk-theme.override {
-      colorVariants = [ "dark" ];
-      tweaks = [ "rimless" "darker" ];
-      themeVariants = [ "orange" ];
-    })
-    papirus-icon-theme
-    bibata-cursors
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.symbols-only
-
-    # Apps
-    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 
   programs = {
